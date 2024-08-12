bf6e7cd6edcbdf9245fa7009b931e3cbfc83e258
RT21sDev <171520861+RT21sDev@users.noreply.github.com>
2024-08-11 20:37:22 -0500

    Update cart.js mk1

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,15 +2,33 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
+
+function addItem(item) {
+    basket.push(item);
+    return true;
+}
 
 
+function listItems() {
+    basket.forEach(item => console.log(item));
+}
 
+function empty() {
+    basket.length = 0;
+}
 
-
-
-
-
-
+// Testing the functions
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now ${basket}`);
+console.log('Adding bananas (expect true)', addItem('bananas'));
+console.log('Adding oranges (expect true)', addItem('oranges'));
+console.log('Listing items:');
+listItems();
+console.log('Emptying the basket');
+empty();
+console.log(`Basket is now ${basket}`);
 
 // DO NOT MODIFY
 // Used for automated testing
